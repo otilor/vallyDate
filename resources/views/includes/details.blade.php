ef3502e277678f95c78818b46f5323d368efd641
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-25 15:42:05 +0100

    Change HTML style

--- resources/views/includes/details.blade.php
+++ resources/views/includes/details.blade.php
@@ -1,11 +1,14 @@
 <form action = "/bvn/verify" method = "post">
     @csrf
   <div class="form-group">
+  	<h5 class=" text-bold" style="font-weight: 800;text-transform: capitalize;">Personal details</h5>
     <label for="exampleFormControlInput1">Name</label>
-    <input  name = "name" class="form-control" id="exampleFormControlInput1" disabled placeholder="{{ auth()->user()->name }}" data-toggle="tooltip" data-placement="top" title="Your details will be verified via your Bank Verification Number">
+    <p><code>{{ auth()->user()->name }}</code></p>
   </div> 
-
+  <hr>
    <div class="form-group">
-    <label for="exampleFormControlInput1">Job details</label>
+    <h5 class=" text-bold" style="font-weight: 800;text-transform: capitalize;">Job details</h5>
+    <p>Application programmer</p>
+
   </div>
 </form>
