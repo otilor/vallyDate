107fb58418987b7e5b94b2a4803f3f1c63161b68
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-25 12:19:57 +0100

    Change class to trait

---
+++ app/Services/AppServices/Bvn/ProcessesBvnDetails.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\AppServices\Bvn;
+
+/**
+ * Process Bvn after getting details from Paystack API
+ */
+trait ProcessesBvnDetails
+{
+	public static function isNameValid($bvnDetails) : bool
+	{
+		//
+	}
+}
