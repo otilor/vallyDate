5eb5b505351e7674da8e5aa2d5fe7052b7219c63
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-25 10:56:03 +0100

    Add tooltip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,23 @@
                 <div class="card-body">
                     <form>
                       <div class="form-group">
-                        <label for="exampleFormControlInput1">Email address</label>
+                        <label for="exampleFormControlInput1">Name</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="John Doe">
+                      </div>
+
+                       <div class="form-group">
+                        <label for="exampleFormControlInput1">Current position</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Software Engineer, No job yet">
+                      </div>
+
+                      <div class="form-group">
+                        <label for="exampleFormControlInput1">Work mail address</label>
                         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
                       </div>
 
                       <div class="form-group">
                         <label for="exampleFormControlInput1">BVN number</label>
-                        <input type="number" maxlength="11" class="form-control" id="exampleFormControlInput1" placeholder="12345678901">
+                        <input type="number" maxlength="11" class="form-control" id="exampleFormControlInput1" placeholder="12345678901" data-toggle="tooltip" data-placement="top" title="Your details will be verified via your Bank Verification Number">
                       </div>
 
                       <div class="form-group">
