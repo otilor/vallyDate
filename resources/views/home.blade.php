6e1476e70aa839f6236919f9822917674492b61b
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-25 11:29:01 +0100

    Change card header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Details panel') }}</div>
+                <div class="card-header">{{ __('Bvn verification') }}</div>
 
                 <div class="card-body">
                     <form action = "/bvn/verify" method = "post">
