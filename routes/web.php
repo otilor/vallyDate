f5685e28c56420adb8b16af3f6a8777ab35e84e4
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-25 09:59:37 +0100

    Add authentication scaffolding

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/paystack', 'Paystack\PaystackController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
