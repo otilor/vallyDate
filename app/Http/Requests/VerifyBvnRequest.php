ff3b275be974843b81e40a0be6499634d79adb06
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-25 11:24:48 +0100

    Add new request class with rule

---
+++ app/Http/Requests/VerifyBvnRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VerifyBvnRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'bvn' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'bvn.required' => 'Your Bank Verification Number is required!',
+        ]
+    }
+}
