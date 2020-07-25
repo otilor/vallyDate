fb5f8946202d8d33fb6d3a3e5e55adb61358643b
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-25 16:05:14 +0100

    Add components to details view

--- resources/views/includes/details.blade.php
+++ resources/views/includes/details.blade.php
@@ -1,17 +1,14 @@
 <section class="random-div bg-not-too-white text-mpgray">
-	<h1 class="text-center py-15px text-35px text-semibold">Your profile</h1>
-	
-	<div class="text-center">
-		<img class = "rounded-img img-responsive" style = "height: 100px; border-radius: 10px; width: 100px;" src="{{ asset('img/male.webp') }}">
+	<p class="card-header alert alert-dark" style="font-size: 18px">Your information</p>
+	<div class="text-center p-2">
+		<img style = "width: 100px; height: 100px; border-radius: 40px;" src="{{ asset('img/male.webp') }}">
 	</div>
-	
-	
 	<div>
-		<h3 class="text-23px pt-5 pb-1 text-mpgray mx-auto random-div-title-description text-center">{{ auth()->user()->name }}</h3>
-
+		<h3 class="text-23px pb-1 text-mpgray mx-auto random-div-title-description text-center">
+			Name: {{ auth()->user()->name }}
+		</h3>
 	<h4 class = "text-center">Your email address:  {{ auth()->user()->email }} </h4>		
 	</div>
 
-	<div class="text-center">
-		<a href = "#" class="btn text-white text-caps btn-info edit">Edit</a>	
-	</div>
+
+
