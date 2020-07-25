d73d37d860c5542e5c332aa6f45402e2058a872a
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-25 12:35:33 +0100

    Return a success message if the BVN is true

--- app/Http/Controllers/BvnController.php
+++ app/Http/Controllers/BvnController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\AppServices\Bvn\ProcessesDetails;
 use App\Services\PaystackService\Bvn;
 use App\Http\Requests\VerifyBvnRequest;
 
 class BvnController extends Controller
 {
+	use ProcessesDetails;
 	function __construct(Bvn $bvn)
 	{
 		$this->bvn = $bvn;
@@ -14,10 +16,15 @@ class BvnController extends Controller
 
     public function verify(VerifyBvnRequest $request)
     {
-    	$resolveBvn = $this->bvn->resolveBvn($request->bvn);
-    	if ($resolveBvn->status  === false)
+    	$resolvedBvn = $this->bvn->resolveBvn($request->bvn);
+    	if ( $resolvedBvn->status  === false )
     	{
     		return back()->withErrors("Invalid BVN!");
+    	}
+
+    	if ( ProcessesDetails::isNameValid($resolvedBvn) )
+    	{
+    		return back()->with('success', 'Successfully verified your BVN!');
     	}
     }
 }
