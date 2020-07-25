8974939e18bc3322165f9d7111788f640220c3cf
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-25 12:16:08 +0100

    Add namespace

--- app/Services/AppServices/Bvn/ProcessBvnDetails.php
+++ app/Services/AppServices/Bvn/ProcessBvnDetails.php
@@ -1,9 +1,11 @@
 <?php
 
+namespace App\Services\AppServices\Bvn;
+
 /**
  * Process Bvn after getting details from Paystack API
  */
-class ProcessBvn
+class ProcessBvnDetails
 {
 	public static function isNameValid($bvnDetails) : bool
 	{
