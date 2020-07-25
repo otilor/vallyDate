b9fe10fdcbf991d900a04b5f026f3edddeaa8f24
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-25 11:20:35 +0100

    Add controller to handle verification of BVN

---
+++ app/Http/Controllers/BvnController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BvnController extends Controller
+{
+    public function verify(Request $request)
+    {
+    	dd ( $Request->all() );
+    }
+}
