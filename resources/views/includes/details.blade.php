4a278c853abad7a275cd96c9ff8a320c2e63c8bf
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-25 15:55:53 +0100

    Add profile image

--- resources/views/includes/details.blade.php
+++ resources/views/includes/details.blade.php
@@ -1,14 +1,21 @@
-<form action = "/bvn/verify" method = "post">
-    @csrf
-  <div class="form-group">
-  	<h5 class=" text-bold" style="font-weight: 800;text-transform: capitalize;">Personal details</h5>
-    <label for="exampleFormControlInput1">Name</label>
-    <p class="text-bold"><code class="text-dark">{{ auth()->user()->name }}</code></p>
-  </div> 
-  <hr>
-   <div class="form-group">
-    <h5 class=" text-bold capitalize">Job details</h5>
-    <p>Application programmer</p>
+<section class="random-div bg-not-too-white text-mpgray">
+<div class="mx-auto text-center text-semibold">
+	<div class="alert alert-success">Your BVN has been verified.</div>
+</div>
 
-  </div>
-</form>
+	<h1 class="text-center py-15px text-35px text-semibold">Your profile</h1>
+	
+	<div class="text-center">
+		<img class = "rounded-img img-responsive" style = "height: 100px; border-radius: 10px; width: 100px;" src="{{ asset('img/male.webp') }}">
+	</div>
+	
+	
+	<div>
+		<h3 class="text-23px pt-5 pb-1 text-mpgray mx-auto random-div-title-description text-center">{{ auth()->user()->name }}</h3>
+
+	<h4 class = "text-center">Your email address:  {{ auth()->user()->email }} </h4>		
+	</div>
+
+	<div class="text-center">
+		<a href = "#" class="btn text-white text-caps btn-info edit">Edit</a>	
+	</div>
