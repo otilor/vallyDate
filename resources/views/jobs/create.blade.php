c5386c9cedb60bc4d41a725a4428a60381ec4820
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-26 15:16:35 +0100

    Remove useless HTML components

---
+++ resources/views/jobs/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Add Job') }}</div>
+
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('jobs.store') }}">
+                        @csrf
+                        <hr>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Company') }}</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control @error('company') is-invalid @enderror" name="company" value="{{ old('company') }}" required autofocus>
+
+                                @error('company')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control"  name="role" value="{{ old('role') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-dark">
+                                    {{ __('Add job') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
