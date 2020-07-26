a183b96498ccdd53fd9f4c53ecfb2f23d9e9d2da
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-26 14:50:58 +0100

    Get the jobs of a particular user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $jobs = auth()->user()->jobs;
+        return view('home', compact('jobs'));
     }
 }
