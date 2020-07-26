85cef47f3ef9a27d7e6fb896dcaa6726d3f8a0ba
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-26 15:23:05 +0100

    Add new request class to validate job creation for a new user

---
+++ app/Http/Requests/VerifyJobDetails.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VerifyJobDetails extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if ( auth()->user()->hasPermission('view details') )
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'company' => 'required',
+            'role' => 'required',
+            'from' => 'required',
+            'to' => 'required',
+        ];
+    }
+}
